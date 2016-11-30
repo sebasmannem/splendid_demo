5dfce9894dfa90b0d727f2f705657caeb3cb294c
S. Mannem <sebastiaan.mannem@splendiddata.com>
2016-11-30 20:34:36 +0100

    Scan all available keys in ansible/roles/ssh/files/add_known_hosts.sh

--- ansible/roles/ssh/files/add_known_hosts.sh
+++ ansible/roles/ssh/files/add_known_hosts.sh
@@ -2,5 +2,5 @@
 set -e
 touch ~/.ssh/known_hosts
 chmod 600 ~/.ssh/known_hosts
-ssh-keyscan -t rsa,dsa "$1" 2>/dev/null | sort -u - ~/.ssh/known_hosts > ~/.ssh/tmp_hosts
+ssh-keyscan "$1" 2>/dev/null | sort -u - ~/.ssh/known_hosts > ~/.ssh/tmp_hosts
 mv ~/.ssh/tmp_hosts ~/.ssh/known_hosts
